62e11a37ff932af27e780ec35f9ecce38de59aa8
Callum <callum.errington@hotmail.co.uk>
2017-11-29 09:54:28 +0000

    Created mixins and funcitons to apply theme and material elevations

---
+++ src/assets/components/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+import LoginForm from './loginForm/loginForm';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <LoginForm/>
+      </div>
+      
+    );
+  }
+}
+
+export default App;
